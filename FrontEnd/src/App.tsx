c8f18d0653e404cbc9db79e6e35a3cd15237156f
Abdelrahman Isler <abdelrahman.isler@gmail.com>
2025-12-07 21:16:22 +0100

    NavBar, MUI, TailwindCSS installation

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,18 +1,20 @@
 //import { useState } from 'react'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
 import About from './Pages/about'
 import Packages from './Pages/Packages'
 import Services from './Pages/Services'
 import HomePage from './Pages/HomePage'
 import Booking from './Pages/Booking'
+import NavBar from './Components/NavBar'
 
 function App() {
   
 
   return (
     <BrowserRouter>
+    
+    <NavBar />
     <Routes>
       <Route path="/" element={<HomePage/>} />
       <Route path="/about" element={<About/>} />
